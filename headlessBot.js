d7f79a467a612baca4b3409a2e6a35986f5d5ded
ICXLeadDev <info@icx.exchange>
2023-02-18 23:18:23 +0000

    updates

--- headlessBot.js
+++ headlessBot.js
@@ -284,7 +284,10 @@ async function twitterLogin (username, password, email, useragent, proxyString) 
                         await page.keyboard.press('Enter');
                         await page.waitForTimeout(30000)
 
-                        const [replyButton] = await page.$x("//span[contains(., 'Reply')]");
+                        //const [replyButton] = await page.$x("//span[contains(., 'Reply')]");
+                        //const [replyButton] = await page.$x("//span[contains(., 'Reply')]");
+                        const replyButton = await page.$x('div[data-testid="tweetButton"]');
+                        // data-testid="tweetButton"
                         if (replyButton) {
                             console.log('Send Reply Button Found!');
                             await replyButton.click();
@@ -480,7 +483,8 @@ async function twitterLogin (username, password, email, useragent, proxyString) 
                         await page.waitForTimeout(10000)
                         await page.keyboard.press('Enter');
                         await page.waitForTimeout(30000)
-                        const [replyButton] = await page.$x("//span[contains(., 'Reply')]");
+                        const replyButton = await page.$x('div[data-testid="tweetButton"]');
+                        //const [replyButton] = await page.$x("//span[contains(., 'Reply')]");
                         if (replyButton) {
                             console.log('Send Reply Button Found!');
                             await replyButton.click();
@@ -665,7 +669,8 @@ async function twitterLogin (username, password, email, useragent, proxyString) 
                         await page.waitForTimeout(10000)
                         await page.keyboard.press('Enter');
                         await page.waitForTimeout(30000)
-                        const [replyButton] = await page.$x("//span[contains(., 'Reply')]");
+                        const replyButton = await page.$x('div[data-testid="tweetButton"]');
+                        //const [replyButton] = await page.$x("//span[contains(., 'Reply')]");
                         if (replyButton) {
                             console.log('Send Reply Button Found!');
                             await replyButton.click();
